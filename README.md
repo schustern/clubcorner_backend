92f2c67a437c3cccedfde7e4a718f4524f8f0189
knattle <jan_malte@t-online.de>
2018-06-28 22:50:28 +0200

    Nutzer können gelöscht werden, wenn sich irgendjemand mit einem gültigen Token verifiziert

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # clubcorner_backend
 Mobile II / ClubCorner / Backend
+
+Ändern des JWT Key in Datei personenController und check-auth
--- node-app/middleware/check-auth.js
+++ node-app/middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        const decoded = jwt.verify(token, "basldjgjsdfhgjksjdfösgjhslfhlj789er7t89eusdhvl");
         req.userData = decoded;
         next();
     } catch (error) {
